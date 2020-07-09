9018e4bc98f2e5b0bab47b8bda79a1ae0bae844d
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-07-09 10:05:38 +0700

    1.1

--- 001.php
+++ 001.php
@@ -1,25 +1,30 @@
 <?php
 
 class Hash {
-    function md5($str)
+    protected $str;
+    function __construct($str)
     {
-        echo md5($str);
+        $this->str = $str;
     }
-    function sha1($str){
-        echo sha1($str);
+    function md5()
+    {
+        echo md5($this->str);
     }
-    function sha256($str){
-        echo hash("sha256", $str);
+    function sha1(){
+        echo sha1($this->str);
     }
-    function sha512($str){
-        echo hash("sha512", $str);
+    function sha256(){
+        echo hash("sha256", $this->str);
+    }
+    function sha512(){
+        echo hash("sha512", $this->str);
     }
 }
 
-$result = new Hash();
-$result->md5("secret");
-$result->sha1("secret");
-$result->sha256("secret");
-$result->sha512("secret");
+$result = new Hash("secret");
+$result->md5();
+$result->sha1();
+$result->sha256();
+$result->sha512();
 
 ?>

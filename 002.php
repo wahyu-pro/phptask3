85ac13587a9c61bbb93ee0ea7e65f040da095054
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-07-09 10:38:11 +0700

    1.1

--- 002.php
+++ 002.php
@@ -1,21 +1,71 @@
 <?php
-
 class Log{
-    function log_info(){
-        $log  = date("Y-m-d H:i:s")." INFO: This is an information about something.".PHP_EOL.
-                date("Y-m-d H:i:s")." ERROR: We can't divide any numbers by zero.".PHP_EOL.
-                date("Y-m-d H:i:s")." NOTICE: Someone loves your status.".PHP_EOL.
-                date("Y-m-d H:i:s")." WARNING: Insufficient funds.".PHP_EOL.
-                date("Y-m-d H:i:s")." DEBUG: This is debug message.".PHP_EOL.
-                date("Y-m-d H:i:s")." ALERT: Achtung! Achtung!".PHP_EOL.
-                date("Y-m-d H:i:s")." CRITICAL: Medic!! We've got critical damages.".PHP_EOL.
-                date("Y-m-d H:i:s")." EMERGENCY: System hung. Contact system administrator immediately!".PHP_EOL;
+    protected $date;
+    function __construct()
+    {
+        $this->date = date("Y-m-d H:i:s");
+    }
 
+    function info($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function error($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function notice($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function warning($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function debug($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function alert($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function critical($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function emergency($message)
+    {
+        $log = $this->date." $message".PHP_EOL;
+        $this->put_contents($log);
+    }
+
+    function put_contents($log){
         file_put_contents("app.log", $log, FILE_APPEND);
     }
 }
 
 $log = new Log();
-$log->log_info();
-
+$log->info("INFO: This is an information about something.");
+$log->error("ERROR: We can't divide any numbers by zero.");
+$log->notice("NOTICE: Someone loves your status.");
+$log->warning("WARNING: Insufficient funds.");
+$log->debug("DEBUG: This is debug message.");
+$log->alert("ALERT: Achtung! Achtung!");
+$log->critical("CRITICAL: Medic!! We've got critical damages.");
+$log->emergency("EMERGENCY: System hung. Contact system administrator immediately!");
 
